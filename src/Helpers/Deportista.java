00c30fa842cd58c87410af10eedd138e0c0e40b9
dhern <dhern@LAPTOP-TC7PLROE>
2023-11-09 16:37:31 -0600

    Actualizando codigo

--- src/Helpers/Deportista.java
+++ src/Helpers/Deportista.java
@@ -4,6 +4,8 @@
 package Helpers;
 
 public class Deportista extends Persona {
+
+    
     // Atributos específicos de la subclase Deportista
     
       public Deportista (){
@@ -15,52 +17,39 @@ public class Deportista extends Persona {
       Establecertelefono("89578998");
     }
     
-    /**
-    private String deporte;
-    private String equipo;
-
-    // Constructor que utiliza el constructor de la superclase Persona
-    public Deportista(String ID, String Nombre, double Edad, String Correo, String Profesion, String Telefono,
-                      String deporte, String equipo) {
-        super.ID = ID;
-        super.Nombre = Nombre;
-        super.Edad = Edad;
-        super.Correo = Correo;
-        super.Profesion = Profesion;
-        super.Telefono = Telefono;
-        this.deporte = deporte;
-        this.equipo = equipo;
-    }
-
+    private String Tipodeporte;
+    private String Equipo;
+  
+    
     // Métodos específicos de la subclase Deportista
-    public void setDeporte(String deporte) {
-        this.deporte = deporte;
+    public void setDeporte(String Tipodeporte) {
+        this.Tipodeporte = Tipodeporte;
     }
 
     public String getDeporte() {
-        return deporte;
+        return this.Tipodeporte;
     }
 
     public void setEquipo(String equipo) {
-        this.equipo = equipo;
+        this.Equipo = Equipo;
     }
 
     public String getEquipo() {
-        return equipo;
+        return this.Equipo;
     }
 
+    /**
     // Método para imprimir información específica de la subclase Deportista
     public void imprimirDeportista() {
-        System.out.println("Deporte: " + deporte);
-        System.out.println("Equipo: " + equipo);
+        System.out.println(("Futbol"));
+        System.out.println("Equipo: " + ("Palma"));
+    }
+*/
+    
+    @Override
+    public String ObternerInformacionPersona() {
+        return "Información del Deportista";
     }
 
-    // Método para imprimir información general de la persona (incluyendo la específica de Deportista)
-    @Override
-    public void imprimirgeneral() {
-        super.imprimirgeneral();
-        imprimirDeportista();
-    }
-    */
 }
 
