5f86545789df34f2fece5e4740e698cbafa37e9a
Kelvin Dariel Muñoz Muñoz <kelvinmunos783@gmail.com>
2023-11-09 12:33:02 -0600

    Primer subida de la clase Deportista, prueba de conexion con Github

---
+++ Trabajo_Grupo_3_Modulo_6/src/trabajo_grupo_3_modulo_6/Deportista.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo_grupo_3_modulo_6;
+
+/**
+ *
+ * @author kelvi
+ */
+public class Deportista {
+    
+}
