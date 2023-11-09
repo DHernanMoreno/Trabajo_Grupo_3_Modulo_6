ad017ae1ea223408273896229c3bea32c44e45d2
Kelvin Dariel Muñoz Muñoz <kelvinmunos783@gmail.com>
2023-11-09 14:46:03 -0600

    Segunda subida ya completada de la subclase Deportista

--- src/Helpers/Deportista.java
+++ src/Helpers/Deportista.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
+
 package Helpers;
 
-/**
- *
- * @author kelvi
- */
-class Deportista {
-    
+public class Deportista extends Persona {
+    // Atributos específicos de la subclase Deportista
+    private String deporte;
+    private String equipo;
+
+    // Constructor que utiliza el constructor de la superclase Persona
+    public Deportista(String ID, String Nombre, double Edad, String Correo, String Profesion, String Telefono,
+                      String deporte, String equipo) {
+        super.ID = ID;
+        super.Nombre = Nombre;
+        super.Edad = Edad;
+        super.Correo = Correo;
+        super.Profesion = Profesion;
+        super.Telefono = Telefono;
+        this.deporte = deporte;
+        this.equipo = equipo;
+    }
+
+    // Métodos específicos de la subclase Deportista
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    // Método para imprimir información específica de la subclase Deportista
+    public void imprimirDeportista() {
+        System.out.println("Deporte: " + deporte);
+        System.out.println("Equipo: " + equipo);
+    }
+
+    // Método para imprimir información general de la persona (incluyendo la específica de Deportista)
+    @Override
+    public void imprimirgeneral() {
+        super.imprimirgeneral();
+        imprimirDeportista();
+    }
 }
+
