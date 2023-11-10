b7a5ff6eeba0f1507f95ad3ac768dddce104bc2c
dhern <dhern@LAPTOP-TC7PLROE>
2023-11-09 23:13:56 -0600

    Actualizando main

--- src/main.java
+++ src/main.java
@@ -13,7 +13,15 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Deportista deportista = new Deportista(); //Instanciar la clase Doctor 
+        
+        
+        System.out.println(deportista.ObternerInformacionPersona());
+        deportista.imprimirgeneral();
+        //Esperando
+        
+        
     }
     
 }
