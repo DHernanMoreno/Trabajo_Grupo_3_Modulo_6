cc2411c36555f960249add9b531d1a7286190408
dhern <dhern@LAPTOP-TC7PLROE>
2023-11-10 00:12:14 -0600

    Limpieza de codigo y impresión

--- src/Helpers/Deportista.java
+++ src/Helpers/Deportista.java
@@ -8,18 +8,8 @@ public class Deportista extends Persona {
     //Subiendo deportista 
     // Atributos específicos de la subclase Deportista
     
-      public Deportista (){
-      Identidad ("1627200089009");
-      EstablecerNombre("Carlos Moreira");
-      EstablecerEdad(27);
-      EstablecerProfesion("Deportista");
-      EstablecerCorreo("C_Moreira@gmail.com");
-      Establecertelefono("89578998");
-    }
-    
     private String Tipodeporte;
-    private String Equipo;
-  
+    private String NEquipo;
     
     // Métodos específicos de la subclase Deportista
     public void setDeporte(String Tipodeporte) {
@@ -30,25 +20,41 @@ public class Deportista extends Persona {
         return this.Tipodeporte;
     }
 
-    public void setEquipo(String equipo) {
-        this.Equipo = Equipo;
+    public void setEquipo(String NEquipo) {
+        this.NEquipo = NEquipo;
     }
 
     public String getEquipo() {
-        return this.Equipo;
+        return this.NEquipo= NEquipo;
     }
-
-    /**
+    
+      public Deportista (){
+      Identidad ("1627200089009");
+      EstablecerNombre("Carlos Moreira");
+      EstablecerEdad(27);
+      EstablecerProfesion("Deportista");
+      EstablecerCorreo("C_Moreira@gmail.com");
+      Establecertelefono("89578998");
+      setDeporte("Futbol");
+      setEquipo("Fuente Azul");
+      
+      
+    }
+    
+    
     // Método para imprimir información específica de la subclase Deportista
     public void imprimirDeportista() {
-        System.out.println(("Futbol"));
-        System.out.println("Equipo: " + ("Palma"));
+        System.out.println("Tipo de Deporte: "+Tipodeporte);
+       System.out.println("Nombre de Equipo de "+Tipodeporte + ":"+ NEquipo);
+       
     }
-*/
+
     
     @Override
     public String ObternerInformacionPersona() {
         return "Información del Deportista";
+        
+       
     }
 
 }
